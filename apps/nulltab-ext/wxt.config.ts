8749633a5155984ca9431e5f418267c2172412be
Steve Zhu <4130171+stevezhu@users.noreply.github.com>
2025-10-28 20:45:48 -0400

    Add name and description to manifest config

--- apps/nulltab-ext/wxt.config.ts
+++ apps/nulltab-ext/wxt.config.ts
@@ -2,5 +2,10 @@ import { defineConfig } from 'wxt';
 
 // See https://wxt.dev/api/config.html
 export default defineConfig({
+  manifest: {
+    name: 'NullTab',
+    description:
+      'A new browsing experience that replaces tab chaos with intelligent organization.',
+  },
   modules: ['@wxt-dev/module-react'],
 });
