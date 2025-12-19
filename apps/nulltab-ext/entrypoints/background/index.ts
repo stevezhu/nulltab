7cc43c55b0560b58825bf0d13737335e77a73e8b
Steve Zhu <4130171+stevezhu@users.noreply.github.com>
2025-12-19 14:55:20 -0500

    Small background script changes

--- apps/nulltab-ext/entrypoints/background/index.ts
+++ apps/nulltab-ext/entrypoints/background/index.ts
@@ -3,22 +3,22 @@ import { defineBackground } from 'wxt/utils/define-background';
 
 import { registerTabService } from '#api/TabService.js';
 
+const DASHBOARD_URL = browser.runtime.getURL('/dashboard.html');
+
 export default defineBackground(() => {
   registerTabService();
 
   // Handle keyboard shortcuts
   browser.commands.onCommand.addListener((command) => {
     if (command === 'open_dashboard') {
-      const dashboardUrl = browser.runtime.getURL('/dashboard.html');
-
-      void browser.tabs.query({ url: dashboardUrl }).then(([tab]) => {
+      void browser.tabs.query({ url: DASHBOARD_URL }).then(([tab]) => {
         if (tab?.id !== undefined) {
           // Dashboard already open, switch to it
           void browser.tabs.update(tab.id, { active: true });
           void browser.windows.update(tab.windowId, { focused: true });
         } else {
           // Dashboard not open, create new tab
-          void browser.tabs.create({ url: dashboardUrl });
+          void browser.tabs.create({ url: DASHBOARD_URL });
         }
       });
     }
