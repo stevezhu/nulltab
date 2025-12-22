d62f6a8ec66de9faca701e12d5a60f25b99f5194
Steve Zhu <4130171+stevezhu@users.noreply.github.com>
2025-12-22 17:49:55 -0500

    Add messaging scaffold

--- apps/nulltab-ext/entrypoints/background/commands.ts
+++ apps/nulltab-ext/entrypoints/background/commands.ts
@@ -7,21 +7,20 @@ const DASHBOARD_URL = browser.runtime.getURL('/dashboard.html');
 
 export async function openDashboard() {
   const [tab] = await browser.tabs.query({ url: DASHBOARD_URL });
-  if (tab?.id !== undefined) {
-    // Dashboard already open, switch to it and send flash message
+  if (!tab) {
+    // Dashboard not open, create new tab
+    await browser.tabs.create({ url: DASHBOARD_URL });
+  } else if (tab.active) {
+    // TODO: not necessarily needed for now
+    // Dashboard is already focused, send message to trigger flash effect
+    // await extensionMessaging.sendMessage('flashTab', undefined, tab.id);
+  } else if (tab.id !== undefined) {
+    // Dashboard already open, switch to it
     const mainTabGroup = await getMainTabGroup();
     await switchTab({
       tabId: tab.id,
       mainTabGroupId: mainTabGroup?.id,
       mainWindowId: mainTabGroup?.windowId,
     });
-
-    // Send message to dashboard to trigger flash effect
-    // void browser.tabs.sendMessage(tab.id, {
-    //   type: 'FLASH_DASHBOARD',
-    // });
-  } else {
-    // Dashboard not open, create new tab
-    await browser.tabs.create({ url: DASHBOARD_URL });
   }
 }
