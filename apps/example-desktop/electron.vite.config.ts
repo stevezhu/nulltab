1c44cbcd2a2b5eb7f0486d033c182fec268944f0
Steve Zhu <4130171+stevezhu@users.noreply.github.com>
2025-12-23 17:09:42 -0500

    Update electron-vite config for v5

--- apps/example-desktop/electron.vite.config.ts
+++ apps/example-desktop/electron.vite.config.ts
@@ -1,21 +1,13 @@
-import { defineConfig, externalizeDepsPlugin } from 'electron-vite';
+import { defineConfig } from 'electron-vite';
 
 import renderer from './vite.config.js';
 
 export default defineConfig({
-  main: {
-    plugins: [
-      externalizeDepsPlugin({
-        // We need to exclude workspace dependencies because they are not built yet so they need
-        // to be included in the build process
-        // exclude: [],
-      }),
-    ],
-  },
+  main: {},
   preload: {
-    // XXX: can't use this with sandboxing because external modules need to be bundled
-    // plugins: [externalizeDepsPlugin()],
     build: {
+      // XXX: can't use this with sandboxing because external modules need to be bundled
+      externalizeDeps: false,
       rollupOptions: {
         output: {
           format: 'cjs', // needed if `"type": "module"` is set in package.json
